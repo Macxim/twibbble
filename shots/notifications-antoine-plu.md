ad832b1763ab0b1a4ddedb4d6fd88c8bccab91c5
Maxime Laforet <me@macx.im>
2020-12-16 07:47:18 +0000

    Add shot 3

---
+++ shots/notifications-antoine-plu.md
@@ -0,0 +1,7 @@
+---
+title: '🔔 Notifications'
+author: 'Antoine Plu'
+originalShotUrl: 'https://dribbble.com/shots/5308459--Notifications'
+originalThumbnailUrl: 'https://cdn.dribbble.com/users/43426/screenshots/5308459/notifications-2.png'
+date: '2020-12-10'
+---
---
+++ src/shots/notifications-antoine-plu/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  // purge: [
+  //   './src/**/*.html',
+  // ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  }
+}
